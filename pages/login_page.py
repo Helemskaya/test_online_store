548bcef09f3ec98a10e98b704dfc1fb6b626d474
Maria <helemskaya@mail.ru>
2025-03-21 15:51:56 +0300

    login page for vasko.ru

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,35 +10,38 @@ class LoginPage(Base):
     def __init__(self, driver):
         super().__init__(driver)
 
-    url = 'https://www.pleza.ru/'  # url тестируемого сайта
-    login_standart_user = 'maria'
+    url = 'https://vasko.ru/'  # url тестируемого сайта
+    login_test_user = 'maria@google.com'
     password_all = '123456'
 
     # Locators
-    log_in = "//a[@href='/login/?backurl=%2F']"
-    user_name = "//input[@name='USER_LOGIN']"
-    password = "//input[@name='USER_PASSWORD']"
-    login_button = "//input[@value='Войти']"
-    main_word = "//a[@href='/personal/profile/?backurl=%2F']"
+    log_in_button = "//a[@id='loginBtn']"
+    user_name = "//input[@id='loginInput']"
+    password = "//input[@id='passwordInput']"
+    authorization_button = "(//button[@class='btn btn_primary'])[1]"
+    user_preview_button = "//a[@id='userPreviewBtn']"
+    main_word = "//b[contains(text(), 'Выход')]"
 
     # Getters
-    def get_log_in(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.log_in)))
+    def get_log_in_button(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.log_in_button)))
 
     def get_user_name(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.user_name)))
     def get_password(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.password)))
-    def get_login_button(self):
-        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.login_button)))
+    def get_authorization_button(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.authorization_button)))
+    def get_user_preview_button(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.user_preview_button)))
 
     def get_main_word(self):
         return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.main_word)))
 
     # Actions
-    def click_log_in(self):
-        self.get_log_in().click()
-        print('Click log in')
+    def click_log_in_button(self):
+        self.get_log_in_button().click()
+        print('Click log in button')
 
     def input_user_name(self, user_name):
         self.get_user_name().send_keys(user_name)
@@ -46,9 +49,12 @@ class LoginPage(Base):
     def input_password(self, password):
         self.get_password().send_keys(password)
         print('Input password')
-    def click_login_button(self):
-        self.get_login_button().click()
-        print('Click login button')
+    def click_authorization_button(self):
+        self.get_authorization_button().click()
+        print('Click authorization button')
+    def click_user_preview_button(self):
+        self.get_user_preview_button().click()
+        print('Click user preview button')
 
     # Methods
     def authorization(self):
@@ -56,8 +62,9 @@ class LoginPage(Base):
         self.driver.get(self.url)
         self.driver.maximize_window()
         self.get_current_url()
-        self.click_log_in()
-        self.input_user_name(self.login_standart_user)    # вызов метода по вводу информации в поле Логин
+        self.click_log_in_button()
+        self.input_user_name(self.login_test_user)    # вызов метода по вводу информации в поле Логин
         self.input_password(self.password_all)   # вызов метода по вводу информации в поле Пароль
-        self.click_login_button()   # клик по кнопке Войти
-        self.assert_word(self.get_main_word(), 'Мария')
+        self.click_authorization_button()   # клик по кнопке Войти
+        self.click_user_preview_button()
+        self.assert_word(self.get_main_word(), 'Выход')
